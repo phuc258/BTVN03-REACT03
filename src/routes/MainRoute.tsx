70f9226ed0832c489ecab34caa28e01f3b7fde11
phuc258 <phuc250805@gmail.com>
2026-01-19 10:09:52 +0700

    student card

--- src/routes/MainRoute.tsx
+++ src/routes/MainRoute.tsx
@@ -5,21 +5,23 @@ import NotFoundPage from "../pages/NotFoundPage";
 import TodoPage from "../pages/TodoPage";
 import LoginPage from "../pages/LoginPage";
 import PrivateRoute from "./PrivateRoute";
+import StudentCardPage from "../pages/StudentCardPage";
 
 const MainRoute: React.FC = () => {
-    return (
-        <BrowserRouter>
-            <Routes>
-                <Route element={<PrivateRoute />}>
-                    <Route path="/todo" element={<TodoPage />} />
-                </Route>
-                
-                <Route path="/login" element={<LoginPage />} />
-                <Route path="/" element={<Navigate to="/login" replace />} />
-                <Route path="*" element={<NotFoundPage />} />
-            </Routes>
-        </BrowserRouter>
-    );
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route element={<PrivateRoute />}>
+          <Route path="/todo" element={<TodoPage />} />
+        </Route>
+
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/" element={<Navigate to="/login" replace />} />
+        <Route path="/student-card" element={<StudentCardPage />} />
+        <Route path="*" element={<NotFoundPage />} />
+      </Routes>
+    </BrowserRouter>
+  );
 };
 
 export default MainRoute;
