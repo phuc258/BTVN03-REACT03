cf1817eee87eb84eee0db849610f360147da97dd
Tran Dinh Phuc <phuc250805@gmail.com>
2026-01-19 11:40:47 +0700

    Revert "feat:test  first commit"

--- src/routes/MainRoute.tsx
+++ src/routes/MainRoute.tsx
@@ -5,23 +5,23 @@ import NotFoundPage from "../pages/NotFoundPage";
 import TodoPage from "../pages/TodoPage";
 import LoginPage from "../pages/LoginPage";
 import PrivateRoute from "./PrivateRoute";
-import UseMemo_Callback from "../pages/UseMemo_Callback";
+import StudentCardPage from "../pages/StudentCardPage";
 
 const MainRoute: React.FC = () => {
-    return (
-        <BrowserRouter>
-            <Routes>
-                <Route element={<PrivateRoute />}>
-                    <Route path="/todo" element={<TodoPage />} />
-                </Route>
-                
-                <Route path="/login" element={<LoginPage />} />
-                <Route path="/" element={<Navigate to="/login" replace />} />
-                <Route path="*" element={<NotFoundPage />} />
-                <Route path="use-memo-callback" element={<UseMemo_Callback />} />
-            </Routes>
-        </BrowserRouter>
-    );
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route element={<PrivateRoute />}>
+          <Route path="/todo" element={<TodoPage />} />
+        </Route>
+
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/" element={<Navigate to="/login" replace />} />
+        <Route path="/student-card" element={<StudentCardPage />} />
+        <Route path="*" element={<NotFoundPage />} />
+      </Routes>
+    </BrowserRouter>
+  );
 };
 
 export default MainRoute;
